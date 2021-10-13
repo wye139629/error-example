51a731e8bd9158fb04d619d088978f95a0a0bc6a
will <wye139629@gmail.com>
2021-10-13 22:59:43 +0800

    add react error boundary

--- src/App.js
+++ src/App.js
@@ -1,46 +1,25 @@
 import React from 'react'
 import Post from './components/Post.js'
+import { ErrorBoundary } from 'react-error-boundary'
 import './css/App.css'
 
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { error: null, errorInfo: null};
+  function ErrorFallback({ error, resetErrorBoundary  }) {
+    return (
+      <div>
+        <p>Something went wrong:</p>
+        <pre>{error}</pre>
+        <button onClick={resetErrorBoundary}>Try again</button>
+      </div>
+    );
   }
-
-  componentDidCatch(error, errorInfo) {
-    // Catch errors in any components below and re-render with error message
-    this.setState({
-      error,
-      errorInfo
-    })
-    // You can also log error messages to an error reporting service here
-  }
-
-  render() {
-    if (this.state.errorInfo) {
-      // Error path
-      return (
-        <div>
-          <h2>Something went wrong.</h2>
-          <details style={{ whiteSpace: 'pre-wrap' }}>
-            {this.state.error && this.state.error.toString()}
-            <br />
-            {this.state.errorInfo.componentStack}
-          </details>
-        </div>
-      );
-    }
-    // Normally, just render children
-    return this.props.children;
-  }
-}
 
 function App() {
   return (
     <div className="App">
       <h1>My post:</h1>
-      <ErrorBoundary>
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+      >
         <Post />
       </ErrorBoundary>
     </div>
